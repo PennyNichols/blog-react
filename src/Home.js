f5a5c111b0d7c8cb40dfaecca84fb42c2b086269
pnichols789 <pnichols789@gmail.com>
2022-08-09 14:34:22 -0500

    useEffect with dependency arrays

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,28 @@
 // start with use
 
 
-import { useState } from 'react';
+// useEffect function runs with every render of the page
+// if you don't want useEffect to run every single time the page is rendered
+// you have to use dependency arrays. An empty array will cause the function to run
+// at the initial render of the page only.
+
+// useEffect(() => {
+//     console.log('use effect ran');
+//     console.log(blogs);
+// }, []);
+
+
+// add state values as dependencies in useEffect
+// function will only run if that state value changes
+
+// useEffect(() => {
+//     console.log('use effect ran');
+//     console.log(blogs);
+// }, [name]);
+
+
+
+import { useEffect, useState } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -16,16 +37,25 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario', id: 3 }
     ]);
 
+    const [name, setName] = useState('mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
-    }
+    };
+
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(name);
+    }, [name]);
 
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'Mario')} title="Mario's Blogs"/>
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'Yoshi')} title="Yoshi's Blogs"/>
+            {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'Mario')} title="Mario's Blogs"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'Yoshi')} title="Yoshi's Blogs"/> */}
+            <button onClick={() => setName('luigi')}>Change Name</button>
+            <p>{ name }</p>
         </div>
     );
 }
